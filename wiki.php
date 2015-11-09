e4c52e5e1808b0b7bae6dd6a9f3b652a2a043d3a
GigaJake <GigaJake@gmail.com>
2015-11-09 16:58:46 -0600

    4th Commit
    added html code

--- wiki.php
+++ wiki.php
@@ -1,17 +1,30 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Wiki</title>
+    </head>
+    <body>
+			<?php
 
-if (file_exists('wiki.txt')) {
-    $content = file_get_contents('wiki.txt');
-} else {
-    $content = '(no content)';
-}
+			if (file_exists('wiki.txt')) {
+					$content = file_get_contents('wiki.txt');
+			} else {
+					$content = '(no content)';
+			}
 
-if (isset($_GET['content'])) {
-    $content = $_GET['content'];
-    file_put_contents('wiki.txt', $content);
-}
+			if (isset($_GET['content'])) {
+					$content = $_GET['content'];
+					file_put_contents('wiki.txt', $content);
+			}
 
-$safe_content = htmlentities($content);
-echo $safe_content;
+			$safe_content = htmlentities($content);
+			echo $safe_content;
 
-?>
+			?>
+			<form action="wiki.php">
+    <textarea name="content" rows="8" cols="80"></textarea>
+    <input type="submit" value="Save">
+</form>
+    </body>
+</html>
