8f15caeb3e5e7bbff6893a6b9202dbddda129a52
GigaJake <GigaJake@gmail.com>
2015-11-11 16:58:01 -0600

    ?

--- wiki.php
+++ wiki.php
@@ -3,16 +3,13 @@
     <head>
         <meta charset="utf-8">
         <title>Wiki</title>
-				<link rel="stylesheet" type="text/css" href="wiki.css">
-				<script src="wiki.js"></script>
     </head>
     <body>
 			<?php
 
 			if (file_exists('wiki.txt')) {
 					$content = file_get_contents('wiki.txt');
-			}
-			else {
+			} else {
 					$content = '(no content)';
 			}
 
@@ -21,20 +18,22 @@
 					file_put_contents('wiki.txt', $content);
 			}
 
-			?>
-			<?php
-				$safe_content = htmlentities($content);
-			?>
-			<div id="content"><?php echo $safe_content; ?></div>
-			<form class="hidden" action="wiki.php">
-    <textarea name="content" rows="8" cols="80">
-			<?php
-
+			$safe_content = htmlentities($content);
 			echo $safe_content;
 
 			?>
-		</textarea>
-    <input type="submit" value="Save">
-</form>
+
+			<form action="wiki.php">
+    		<textarea name="content" rows="8" cols="80">
+					<?php echo $safe_content; ?>
+				</textarea>
+    		<input type="submit" value="Save">
+			</form>
+
+				<?php $safe_content = htmlentities($content); ?>
+			<div id="content">
+    		<?php echo $safe_content; ?>
+			</div>
+			
     </body>
 </html>
